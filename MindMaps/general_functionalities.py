f61f820c0c3ae12b978e4429515210b260f54296
John Capobianco <ptcapo@gmail.com>
2021-12-22 10:53:24 -0500

    ospf

--- MindMaps/general_functionalities.py
+++ MindMaps/general_functionalities.py
@@ -7,3 +7,13 @@ class ParseShowCommandFunction:
             except Exception as e:
                 step.failed('Could not parse it correctly\n{e}'.format(e=e))
                 return None
+
+class ParseLearnFunction:
+    @staticmethod
+    def parse_learn(steps, device, function_name: str):
+            with steps.start(f'Learning {function_name}',continue_=True) as step:
+                try:
+                    return device.learn(function_name).info
+                except Exception as e:
+                    step.failed('Could not learn it correctly\n{e}'.format(e=e))
+                    return None                
